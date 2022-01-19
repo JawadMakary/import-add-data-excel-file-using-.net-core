a9a8470aee613f0be6450d4c8501beda0037efc9
jawad halby makary <jawadhalbymakary@gmail.com>
2022-01-19 13:39:01 +0200

    added import functionality

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// import excel library
+using ClosedXML.Excel;
+using var wbook = new XLWorkbook("products.xlsx");
+
+var ws1 = wbook.Worksheet(1); 
+// get all data using forEach
+foreach (var row in ws1.Rows())
+{
+    foreach (var cell in row.Cells())
+    {
+        Console.Write(cell.Value.ToString() + " ");
+    }
+    Console.WriteLine();
+}
